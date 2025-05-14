d79ee0edfc8c5181034d1f6a059afae0d88fb842
fastube <120088732+fastube@users.noreply.github.com>
2025-05-14 20:15:36 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,6 +6,8 @@ const $caches = [
   "./style.css",
   "./script.js",
   "./beli.svg",
+  "./gopay.svg",
+  "./dana.svg",
   "https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100;8..144,200;8..144,300;8..144,400;8..144,500;8..144,600;8..144,700;8..144,800;8..144,900;8..144,1000&display=swap",
   "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css",
   "https://i.postimg.cc/pd23ZhDH/icon.png",
